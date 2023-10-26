69be1dd982c6c7380468444fef1738070f016159
Zigguraptor <cobaltum8@gmail.com>
2023-10-26 23:52:28 +0400

    added GetFriendsAsync()

--- UsersManager.Application/Interfaces/IFriendshipRepository.cs
+++ UsersManager.Application/Interfaces/IFriendshipRepository.cs
@@ -1,9 +1,11 @@
 ﻿using UsersManager.Application.Friendship.Commands.FriendRequest;
+using UsersManager.Application.Friendship.Queries;
 
 namespace UsersManager.Application.Interfaces;
 
 public interface IFriendshipRepository
 {
+    public Task<IEnumerable<FriendVm>> GetFriendsAsync(string userName);
     public Task<bool> FriendshipExistsAsync(Guid user1Uuid, Guid user2Uuid);
     public Task FriendInviteAsync(FriendInviteDto friendInviteDto);
 }
--- UsersManager.Persistence/FriendshipRepository.cs
+++ UsersManager.Persistence/FriendshipRepository.cs
@@ -1,6 +1,7 @@
 ﻿using System.Data;
 using Dapper;
 using UsersManager.Application.Friendship.Commands.FriendRequest;
+using UsersManager.Application.Friendship.Queries;
 using UsersManager.Application.Interfaces;
 
 namespace UsersManager.Persistence;
@@ -9,9 +10,35 @@ public class FriendshipRepository : IFriendshipRepository
 {
     private readonly IDbConnection _dbConnection;
 
-    public FriendshipRepository(IDbConnection dbConnection)
-    {
+    public FriendshipRepository(IDbConnection dbConnection) =>
         _dbConnection = dbConnection;
+
+    public Task<IEnumerable<FriendVm>> GetFriendsAsync(string userName)
+    {
+        userName = userName.ToLower();
+
+        const string sql = """
+                           WITH "friends_uuids"
+                                    AS (
+                                       SELECT "User2Uuid" AS "uuid"
+                                       FROM "UsersFriends" "uf"
+                                           JOIN "Users" "u"
+                                               ON "User1Uuid" = "u"."Uuid"
+                                       WHERE lower("u"."UserName") = @userName
+                                       UNION ALL
+                                       SELECT "User1Uuid" AS "uuid"
+                                       FROM "UsersFriends" "uf"
+                                           JOIN "Users" "u"
+                                               ON "User2Uuid" = "u"."Uuid"
+                                       WHERE lower("u"."UserName") = @userName
+                                       )
+                           SELECT "u".*
+                           FROM "friends_uuids" "fu"
+                               JOIN "Users" "u"
+                                   ON "u"."Uuid" = "fu"."uuid"
+                                   AND "u"."IsActive";
+                           """;
+        return _dbConnection.QueryAsync<FriendVm>(sql, new { userName });
     }
 
     public Task<bool> FriendshipExistsAsync(Guid user1Uuid, Guid user2Uuid)
