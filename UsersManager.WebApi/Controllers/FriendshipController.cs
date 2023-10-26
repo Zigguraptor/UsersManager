02d8e27f552a58b33358b6b279947cdbce69aff6
Zigguraptor <cobaltum8@gmail.com>
2023-10-26 22:20:09 +0400

    added FriendshipController.cs

---
+++ UsersManager.WebApi/Controllers/FriendshipController.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+using UsersManager.Application.Friendship.Commands.FriendRequest;
+
+namespace UsersManager.WebApi.Controllers;
+
+public sealed class FriendshipController : BaseController
+{
+    private readonly ISender _sender;
+
+    public FriendshipController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> SendFriendInviteAsync([FromBody] FriendInviteDto friendInviteDto)
+    {
+        if (!ModelState.IsValid)
+            return UnprocessableEntity(ModelState);
+
+        var command = new FriendInviteCommand(friendInviteDto);
+        try
+        {
+            if (await _sender.Send(command))
+                return Ok();
+
+            return BadRequest("Пользователи уже друзья");
+        }
+        catch (Exception e)
+        {
+            if (e is PostgresException { SqlState: "23505" })
+                return BadRequest("Запрос уже отправлен");
+
+            throw;
+        }
+    }
+}
