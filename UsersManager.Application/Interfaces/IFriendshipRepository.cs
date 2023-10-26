84575550641d2c7de2c4f3f6811d4069cd2a3a3f
Zigguraptor <cobaltum8@gmail.com>
2023-10-26 22:17:24 +0400

    added FriendshipRepository.cs

---
+++ UsersManager.Application/Interfaces/IFriendshipRepository.cs
@@ -0,0 +1,9 @@
+﻿using UsersManager.Application.Friendship.Commands.FriendRequest;
+
+namespace UsersManager.Application.Interfaces;
+
+public interface IFriendshipRepository
+{
+    public Task<bool> FriendshipExistsAsync(Guid user1Uuid, Guid user2Uuid);
+    public Task FriendInviteAsync(FriendInviteDto friendInviteDto);
+}
---
+++ UsersManager.Persistence/FriendshipRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Data;
+using Dapper;
+using UsersManager.Application.Friendship.Commands.FriendRequest;
+using UsersManager.Application.Interfaces;
+
+namespace UsersManager.Persistence;
+
+public class FriendshipRepository : IFriendshipRepository
+{
+    private readonly IDbConnection _dbConnection;
+
+    public FriendshipRepository(IDbConnection dbConnection)
+    {
+        _dbConnection = dbConnection;
+    }
+
+    public Task<bool> FriendshipExistsAsync(Guid user1Uuid, Guid user2Uuid)
+    {
+        const string query = "SELECT \"public\".\"is_friends_func\"(@user1Uuid, @user2Uuid);";
+        return _dbConnection.QueryFirstAsync<bool>(query, new { user1Uuid, user2Uuid });
+    }
+
+    public Task FriendInviteAsync(FriendInviteDto friendInviteDto)
+    {
+        const string sql = "CALL friend_invite_proc(@FromUserUuid, @ToUserUuid);";
+        return _dbConnection.ExecuteAsync(sql, friendInviteDto);
+    }
+}
