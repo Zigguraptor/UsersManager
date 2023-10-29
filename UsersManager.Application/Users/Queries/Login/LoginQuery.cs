474c52ec10ed59c51c469a1f3c4b49883be6a889
Zigguraptor <cobaltum8@gmail.com>
2023-10-29 04:30:42 +0400

    added LoginQuery

---
+++ UsersManager.Application/Users/Queries/Login/LoginQuery.cs
@@ -0,0 +1,15 @@
+﻿using MediatR;
+
+namespace UsersManager.Application.Users.Queries.Login;
+
+public class LoginQuery : IRequest<string?>
+{
+    public LoginQuery(string userName, string password)
+    {
+        UserName = userName;
+        Password = password;
+    }
+
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
---
+++ UsersManager.Application/Users/Queries/Login/LoginQueryHandler.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using UsersManager.Application.Interfaces;
+
+namespace UsersManager.Application.Users.Queries.Login;
+
+public class LoginQueryHandler : IRequestHandler<LoginQuery, string?>
+{
+    private readonly IPasswordHandler _passwordHandler;
+    private readonly ITokenGenerator _tokenGenerator;
+    private readonly IUsersRepository _usersRepository;
+
+    public LoginQueryHandler(IPasswordHandler passwordHandler, ITokenGenerator tokenGenerator,
+        IUsersRepository usersRepository)
+    {
+        _passwordHandler = passwordHandler;
+        _tokenGenerator = tokenGenerator;
+        _usersRepository = usersRepository;
+    }
+
+    public async Task<string?> Handle(LoginQuery request, CancellationToken cancellationToken)
+    {
+        var user = await _usersRepository.GetUserAsync(userName: request.UserName);
+        if (user == null) throw new ArgumentException();
+        if (_passwordHandler.ValidatePassword(request.Password, user.PasswordHash))
+            return _tokenGenerator.GenerateToken(user);
+
+        return null;
+    }
+}
