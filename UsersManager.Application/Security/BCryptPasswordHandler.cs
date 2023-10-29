96b189c2a7b4e9bf11d5439d108ca5f98874a80b
Zigguraptor <cobaltum8@gmail.com>
2023-10-29 04:30:20 +0400

    ups

---
+++ UsersManager.Application/Security/BCryptPasswordHandler.cs
@@ -0,0 +1,9 @@
+﻿using UsersManager.Application.Interfaces;
+
+namespace UsersManager.Application.Security;
+
+public class BCryptPasswordHandler : IPasswordHandler
+{
+    public string HashPassword(string password) => BCrypt.Net.BCrypt.HashPassword(password);
+    public bool ValidatePassword(string password, string hash) => BCrypt.Net.BCrypt.Verify(password, hash);
+}
---
+++ UsersManager.Application/Security/JwtTokenGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using UsersManager.Application.Interfaces;
+using UsersManager.Domain;
+
+namespace UsersManager.Application.Security;
+
+public class JwtTokenGenerator : ITokenGenerator
+{
+    public string GenerateToken(User user)
+    {
+        var claims = new List<Claim>
+        {
+            new(ClaimTypes.Name, user.Uuid.ToString()),
+        };
+
+        var jwt = new JwtSecurityToken(
+            audience: "text-audience.com",
+            issuer: "test-issuer.com",
+            expires: DateTime.Now.AddHours(1),
+            claims: claims,
+            signingCredentials: new SigningCredentials(
+                new SymmetricSecurityKey("eb58ed4d-46e3-46a5-ab25-55cb1412f1e6"u8.ToArray()),
+                SecurityAlgorithms.HmacSha256));
+
+        return new JwtSecurityTokenHandler().WriteToken(jwt);
+    }
+}
