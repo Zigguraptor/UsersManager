74f9836b67dea2afea492252f87beb9ff6170f6d
Zigguraptor <cobaltum8@gmail.com>
2023-10-29 04:31:16 +0400

    added Authentication and Authorization

---
+++ UsersManager.WebApi/AuthenticationAndAuthorizationExtensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
+namespace UsersManager.WebApi;
+
+public static class AuthenticationAndAuthorizationExtensions
+{
+    public static IServiceCollection AddCustomAuthenticationAndAuthorization(this IServiceCollection services,
+        ConfigurationManager builderConfiguration)
+    {
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                RequireExpirationTime = false,
+                RequireSignedTokens = true,
+                RequireAudience = false,
+                SaveSigninToken = false,
+                TryAllIssuerSigningKeys = true,
+                ValidateActor = false,
+                ValidateAudience = false,
+                ValidateIssuer = false,
+                ValidateLifetime = false,
+                ValidateTokenReplay = false,
+
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey("eb58ed4d-46e3-46a5-ab25-55cb1412f1e6"u8.ToArray())
+            };
+        });
+        
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy("RequireAdministratorRole",
+                policyBuilder => policyBuilder.RequireRole("Administrator"));
+        });
+
+        return services;
+    }
+}
--- UsersManager.WebApi/Program.cs
+++ UsersManager.WebApi/Program.cs
@@ -1,9 +1,13 @@
 using System.Reflection;
+using Microsoft.OpenApi.Models;
 using UsersManager.Application;
 using UsersManager.Application.Common.Mappings;
 using UsersManager.Persistence;
+using UsersManager.WebApi;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCustomAuthenticationAndAuthorization(builder.Configuration);
 
 builder.Services.AddApplication();
 builder.Services.AddPersistence(builder.Configuration);
@@ -17,19 +21,51 @@ builder.Services.AddAutoMapper(config =>
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1",
+        new OpenApiInfo
+        {
+            Title = "Users manager api",
+            Version = "v1",
+        });
+
+    options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.ApiKey,
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Scheme = "Bearer",
+    });
+
+    options.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 var app = builder.Build();
+
+// app.UseHttpsRedirection();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-// app.UseHttpsRedirection();
-
-// app.UseAuthorization();
 
 app.MapControllers();
 
