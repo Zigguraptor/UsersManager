383597d5ddb5af88bd90abbf13173935d4b483b4
Zigguraptor <cobaltum8@gmail.com>
2023-10-29 04:33:56 +0400

    added password

--- UsersManager.Application/Users/Commands/CreateUser/CreateUserCommandHandler.cs
+++ UsersManager.Application/Users/Commands/CreateUser/CreateUserCommandHandler.cs
@@ -9,17 +9,20 @@ public class CreateUserCommandHandler : IRequestHandler<CreateUserCommand, Guid>
 {
     private readonly IMapper _mapper;
     private readonly IUsersRepository _usersRepository;
+    private readonly IPasswordHandler _passwordHandler;
 
-    public CreateUserCommandHandler(IMapper mapper, IUsersRepository usersRepository)
+    public CreateUserCommandHandler(IMapper mapper, IUsersRepository usersRepository, IPasswordHandler passwordHandler)
     {
         _mapper = mapper;
         _usersRepository = usersRepository;
+        _passwordHandler = passwordHandler;
     }
 
     public Task<Guid> Handle(CreateUserCommand request, CancellationToken cancellationToken)
     {
         var user = _mapper.Map<User>(request.CreateUserDto);
-
+        user.PasswordHash = _passwordHandler.HashPassword(request.CreateUserDto.Password);
+        
         return _usersRepository.InsertUserAsync(user);
     }
 }
