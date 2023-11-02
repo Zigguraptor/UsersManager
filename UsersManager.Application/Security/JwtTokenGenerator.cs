5b48d667804b053ea9331c6c55526c3faf90bfaa
Zigguraptor <cobaltum8@gmail.com>
2023-11-02 21:29:49 +0400

    added DateOfBirth

--- UsersManager.Application/Security/JwtTokenGenerator.cs
+++ UsersManager.Application/Security/JwtTokenGenerator.cs
@@ -1,5 +1,7 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Text;
+using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using UsersManager.Application.Interfaces;
 using UsersManager.Domain;
@@ -8,25 +10,39 @@ namespace UsersManager.Application.Security;
 
 public class JwtTokenGenerator : ITokenGenerator
 {
+    private readonly IConfiguration _configuration;
+
+    public JwtTokenGenerator(IConfiguration configuration) => _configuration = configuration;
+
     public string GenerateToken(User user, bool isAdmin = false)
     {
         var claims = new List<Claim>
         {
             new(ClaimTypes.Name, user.Uuid.ToString()),
+            new(ClaimTypes.DateOfBirth, user.Dob.ToString())
         };
 
         if (isAdmin)
             claims.Add(new Claim(ClaimTypes.Role, "Administrator"));
 
-        var jwt = new JwtSecurityToken(
-            audience: "text-audience.com",
-            issuer: "test-issuer.com",
-            expires: DateTime.Now.AddHours(1),
-            claims: claims,
-            signingCredentials: new SigningCredentials(
-                new SymmetricSecurityKey("eb58ed4d-46e3-46a5-ab25-55cb1412f1e6"u8.ToArray()),
-                SecurityAlgorithms.HmacSha256));
+        var expireDate = DateTime.Now.AddSeconds(_configuration.GetValue<double>("DefaultJwtTokenLiveTimeS"));
 
-        return "Bearer " + new JwtSecurityTokenHandler().WriteToken(jwt);
+        var jwt = new JwtSecurityToken(
+            expires: expireDate,
+            claims: claims,
+            signingCredentials: CreateSigningCredentials());
+
+        _configuration.Bind("DefaultJwtSecurityToken", jwt);
+
+        return new JwtSecurityTokenHandler().WriteToken(jwt);
+    }
+
+    private SigningCredentials CreateSigningCredentials()
+    {
+        var secretKey = Encoding.UTF8.GetBytes(
+            _configuration.GetValue<string>("TokenValidationSecurityKey")
+            ?? throw new InvalidOperationException());
+
+        return new SigningCredentials(new SymmetricSecurityKey(secretKey), SecurityAlgorithms.HmacSha256);
     }
 }
