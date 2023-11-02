09beb3ce5d3fee80e34bb5b40e666f2fb0e682dd
Zigguraptor <cobaltum8@gmail.com>
2023-11-02 22:12:56 +0400

    added ContentController.cs

---
+++ UsersManager.WebApi/Controllers/ContentController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UsersManager.WebApi.Controllers;
+
+public class ContentController : BaseController
+{
+    [HttpGet]
+    [Authorize("Adult")]
+    public IActionResult AdultContentAsync() => Ok("The earth is flat.");
+}
