08916a20091a3e0a13430dbbe1703f0161635239
Zigguraptor <cobaltum8@gmail.com>
2023-11-12 15:34:14 +0400

    added logger

--- UsersManager.WebApi/HttpExceptionMiddleware.cs
+++ UsersManager.WebApi/HttpExceptionMiddleware.cs
@@ -4,9 +4,14 @@ namespace UsersManager.WebApi;
 
 public class HttpExceptionMiddleware
 {
+    private readonly ILogger<HttpExceptionMiddleware> _logger;
     private readonly RequestDelegate _next;
 
-    public HttpExceptionMiddleware(RequestDelegate next) => _next = next;
+    public HttpExceptionMiddleware(ILogger<HttpExceptionMiddleware> logger, RequestDelegate next)
+    {
+        _logger = logger;
+        _next = next;
+    }
 
     public async Task InvokeAsync(HttpContext context)
     {
@@ -21,6 +26,10 @@ public class HttpExceptionMiddleware
             context.Response.Clear();
             context.Response.StatusCode = ex.StatusCode;
             context.Response.ContentType = "text/plain";
+            
+            _logger.LogInformation("Handled http exception to ip:{ipAddress}; status code: {code}; {message}",
+                context.Connection.RemoteIpAddress?.ToString(), ex.StatusCode, ex.ResponseMessage);
+            
             if (ex.ResponseMessage != null)
                 await context.Response.WriteAsync(ex.ResponseMessage);
         }
