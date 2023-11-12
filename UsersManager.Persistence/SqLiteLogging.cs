020318eb6ebfb4f3974d027d6be8ba8d83729d7e
Zigguraptor <cobaltum8@gmail.com>
2023-11-12 21:54:36 +0400

    added log method sqlite

--- UsersManager.Persistence/SqLiteLogging.cs
+++ UsersManager.Persistence/SqLiteLogging.cs
@@ -21,4 +21,23 @@ public static class SqLiteLogging
         using var command = new SQLiteCommand(createTableQuery, connection);
         command.ExecuteNonQuery();
     }
+
+    // ReSharper disable once UnusedMember.Global
+    public static async Task LogAsync(string connectionString, string timestamp, string level, string logger,
+        string message)
+    {
+        await using var connection = new SQLiteConnection(connectionString);
+        connection.Open();
+
+        await using var command = new SQLiteCommand(connection);
+        command.CommandText =
+            "INSERT INTO Log(TimeStamp, Level, Logger, Message) VALUES (@Timestamp, @Level, @Logger, @Message)";
+
+        command.Parameters.AddWithValue("@Timestamp", timestamp);
+        command.Parameters.AddWithValue("@Level", level);
+        command.Parameters.AddWithValue("@Logger", logger);
+        command.Parameters.AddWithValue("@Message", message);
+
+        command.ExecuteNonQuery();
+    }
 }
